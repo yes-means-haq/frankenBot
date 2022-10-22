a811674f19d12f21213928b33f21c54be48e54fe
yes-mean-haq <yes.means.haq@gmail.com>
2022-10-22 20:43:48 +0200

    listenner

--- Motion.py
+++
@@ -1,7 +0,0 @@
-class Motion:
-    
-    def __init__(self):
-        print("Instance")
-    
-    def moveForward():
-        return " - Forward - "
--- event.py
+++
@@ -1,8 +0,0 @@
-import keyboard  
-while True:  
-    try:  
-        if keyboard.is_pressed('up'):  
-            print('You Pressed up!')
-            break  
-    except:
-        break  
