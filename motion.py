6918d29c3820d3a2da68da1d0b32d35eb9f37a75
Nassim <nas.eam@gmail.com>
2023-04-08 14:42:15 +0200

    update to fix send event from terminal

--- motion.py
+++ motion.py
@@ -1,57 +1,19 @@
-import time
-import curses
-import readchar
 from gpiozero import CamJamKitRobot
-
-# Pour executer : cd ~/Documents/frankenbot && sudo python3 motion.py
+from sshkeyboard import listen_keyboard
 
 robot = CamJamKitRobot()
 
-# Initialize the curses library
-stdscr = curses.initscr()
-curses.cbreak()
-stdscr.keypad(True)
+def on_press(key):
+    if key == 'up':
+        robot.forward()
+    elif key == 'down':
+        robot.backward()
+    elif key == 'left':
+        robot.left()
+    elif key == 'right':
+        robot.right()
 
-def cleanupCurses():
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-
-
-
-try:
-    while True:
-        # Read a single character from the console input
-        char = stdscr.getch()
-        char = chr(char)
-        print(char)
-        print("\n")
-
-        if char == 'Ă':
-            robot.forward()
-        elif char == 'ă':
-            robot.backward(1)
-        elif char == 'Ą':
-            robot.left(0.3)
-        elif char == 'ą':
-            robot.right(0.3)
-        elif char == 's':
-            robot.stop()
-        elif char == 'q':
-                break
-        else:
-            robot.stop()
-
-    robot.stop() # stop bot before leave
-
-    # Clean up the curses library
-    cleanupCurses()
-
-except KeyboardInterrupt:
+def on_release(key):
     robot.stop()
-    print("\nKill program")
 
-    # Clean up the curses library
-    cleanupCurses()
-
+listen_keyboard(on_press=on_press, on_release=on_release)
